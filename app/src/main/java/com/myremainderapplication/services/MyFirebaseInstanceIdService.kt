5ec8e760a823b64d491a1afd12012d13e746987e
shubham chauhan <shubham.chauhan@kelltontech.com>
2017-12-21 14:03:14 +0530

    + initial commit

---
+++ app/src/main/java/com/myremainderapplication/services/MyFirebaseInstanceIdService.kt
@@ -0,0 +1,22 @@
+package com.myremainderapplication.services
+
+import android.content.ContentValues
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+/**
+ * Created by user on 21/12/17.
+ */
+class MyFirebaseInstanceIdService : FirebaseInstanceIdService() {
+    override fun onTokenRefresh() {
+        // Get updated InstanceID token.
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.d(ContentValues.TAG, "Refreshed token: " + refreshedToken!!)
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(refreshedToken)
+    }
+}
